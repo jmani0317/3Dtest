7cc32fedf5a6a0c1ddfda942ec0bc860bc72ab4d
Joshua Manibusan <manibusan.joshua06@gmail.com>
2025-03-07 13:32:19 -0500

    Created windows procedure and termination message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+		case WM_CLOSE:
+			PostQuitMessage(10);
+			break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE	hInstance,
 	HINSTANCE	hPrevInstance,
@@ -10,7 +21,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof( wc );
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -32,6 +43,23 @@ int CALLBACK WinMain(
 		nullptr
 	);
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
+
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+
 	return 0;
 }
